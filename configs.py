d49b049ca87bde7dda38e631dc44853c5ecb7b9f
draxxd30 <70913893+draxxd30@users.noreply.github.com>
2023-02-26 06:13:48 -0300

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,9 +4,9 @@ class config:
     API_ID = int(getenv("API_ID",'24054386'))
     API_HASH = getenv("API_HASH",'6684db1e16988756df0d5c12ad3e6b09')
     BOT_TOKEN = getenv("BOT_TOKEN",'5895609920:AAGZML4udfhG2TfSft_jgL5Cds0Rgnkq_ks')
-    DB_HOST = 'localhost' 
-    DB_USER = 'root' 
-    DB_PASS = '' 
-    DB_NAME = 'accounts'
+    DB_HOST = 'mysql-draxx.alwaysdata.net' 
+    DB_USER = 'draxx' 
+    DB_PASS = 'Panaburguer446' 
+    DB_NAME = 'draxx_accounts'
 
 config = config()
